ac19942e023e4b667eee7395e31d34e7bf8be2e9
julichow <julichow94@gmail.com>
2023-05-10 01:01:05 +0800

    updates on req.query for department

--- routes/employees.js
+++ routes/employees.js
@@ -16,7 +16,7 @@ GET employees list
 */ 
 router.get("/employees", async (req, res, next) =>{
   try {
-    let results = await db("SELECT p.employeeId, p.fullName, p.address, p.country, p.passport, p.emailAddress, p.birthDate, p.phoneNumber, p.maritalStatus, w.department, w.epfNumber, w.SOCSO, w.startDate FROM personal_info p JOIN work_info w ON p.employeeID = w.employeeId;");
+    let results = await db('SELECT p.employeeId, p.fullName, p.address, p.country, p.passport, p.emailAddress, p.birthDate, p.phoneNumber, p.maritalStatus, w.department, w.epfNumber, w.SOCSO, w.startDate FROM personal_info p JOIN work_info w ON p.employeeID = w.employeeId;');
     // console.log("RESULTS: ", results)
     res.send(results.data);
   } catch (err) {
@@ -55,20 +55,21 @@ GET one employee using query
 4. wrap quote around '${department}' because it is a string 
 5. view data via postman GET: localhost:4000/api/employees?department='${department}'
 */ 
-router.get("/employees", async (req, res, next) => {
-  // let department = req.query.department
-  let { department } = req.query;
-  console.log(req.query)
+router.get("/", async (req, res, next) => { 
   try {
-    let results = await db(`SELECT p.employeeId, p.fullName, p.address, p.country, p.passport, p.emailAddress, p.birthDate, p.phoneNumber, p.maritalStatus, w.department, w.epfNumber, w.SOCSO, w.startDate FROM personal_info p JOIN work_info w ON p.employeeID = w.employeeId WHERE w.department='${department}'`);
+    if (req.query.department) {
+    let results = await db(`SELECT p.employeeId, p.fullName, p.address, p.country, p.passport, p.emailAddress, p.birthDate, p.phoneNumber, p.maritalStatus, w.department, w.epfNumber, w.SOCSO, w.startDate FROM personal_info p JOIN work_info w ON p.employeeID = w.employeeId WHERE w.department='${req.query.department}'`);
+  //get the updated list of employee
+      res.send(results.data);
+  } else {
+      let results = await db('SELECT p.employeeId, p.fullName, p.address, p.country, p.passport, p.emailAddress, p.birthDate, p.phoneNumber, p.maritalStatus, w.department, w.epfNumber, w.SOCSO, w.startDate FROM personal_info p JOIN work_info w ON p.employeeID = w.employeeId;');
+      res.send(results.data);
+    }
     // console.log("RESULTS: ", results)
-    //get the updated list of employee
-    res.send(results.data);
   } catch (err) {
     res.status(500).send({ error: err.message });
   }
 });
-
 
 /*
 INSERT a new employee into personal_info
@@ -77,33 +78,34 @@ INSERT a new employee into personal_info
 3. view data via postman POST: localhost:4000/api/employees
 */
 router.post("/employees", async (req, res, next) => {
-  let { employeeId, fullName, address, country, passport, emailAddress, birthDate, phoneNumber, maritalStatus } = req.body;
+  const { employeeId, fullName, address, country, passport, emailAddress, birthDate, phoneNumber, maritalStatus, department, epfNumber, SOCSO, startDate } = req.body;
   // console.log(req.body)
   try {
     //insert new employee. Id will be auto incremented
     await db(`INSERT INTO personal_info ( employeeId, fullName, address, country, passport, emailAddress, birthDate, phoneNumber, maritalStatus) VALUES ('${employeeId}', '${fullName}', '${address}', '${country}', '${passport}', '${emailAddress}', '${birthDate}', '${phoneNumber}', '${maritalStatus}');`);
+    //insert new employee. Id will be auto incremented
+     await db(`INSERT INTO work_info(employeeId, department, epfNumber, SOCSO, startDate) VALUES ('${employeeId}', '${department}', '${epfNumber}', '${SOCSO}', '${startDate}');`);
     //get the updated list of students
-    let results = await db('SELECT * FROM personal_info;');
+    let results = await db('SELECT p.employeeId, p.fullName, p.address, p.country, p.passport, p.emailAddress, p.birthDate, p.phoneNumber, p.maritalStatus, w.department, w.epfNumber, w.SOCSO, w.startDate FROM personal_info p JOIN work_info w ON p.employeeID = w.employeeId;');
     res.send(results.data);
   } catch (err) {
     res.status(500).send({ error: err.message });
   }
 });
 
-/*
-INSERT a new employee into work_info
-1. create personal_info & work_info table respectively
-2. join both tables
-3. view data via postman POST: localhost:4000/api/employees
-*/
-router.post("/employees", async (req, res, next) => {
-  let { employeeId, department, epfNumber, SOCSO, startDate } = req.body;
-  // console.log(req.body)
+// PUT an employee
+//1. write syntax for DELETE FROM personal via mySQL and copy paste here
+//2. view data via postman DELETE: localhost:4000/api/employees/:id
+router.put("employees/:id", async function(req, res, next) {
+  //let id = req.params.id
+  const { employeeId, fullName, address, country, passport, emailAddress, birthDate, phoneNumber, maritalStatus, department, epfNumber, SOCSO, startDate } = req.body;
   try {
-    //insert new employee. Id will be auto incremented
-    await db(`INSERT INTO work_info(employeeId, department, epfNumber, SOCSO, startDate) VALUES ('${employeeId}', '${department}', '${epfNumber}', '${SOCSO}', '${startDate}');`);
+    //delete students with specified id from database
+    await db(`DELETE FROM personal_info WHERE id = ${id};`); 
+     //delete students with specified id from database
+     await db(`DELETE FROM work_info WHERE id = ${id};`); 
     //get the updated list of students
-    let results = await db('SELECT * FROM work_info;');
+    let results = await db('SELECT p.employeeId, p.fullName, p.address, p.country, p.passport, p.emailAddress, p.birthDate, p.phoneNumber, p.maritalStatus, w.department, w.epfNumber, w.SOCSO, w.startDate FROM personal_info p JOIN work_info w ON p.employeeID = w.employeeId;');
     res.send(results.data);
   } catch (err) {
     res.status(500).send({ error: err.message });
@@ -113,43 +115,21 @@ router.post("/employees", async (req, res, next) => {
 // DELETE an employee
 //1. write syntax for DELETE FROM personal via mySQL and copy paste here
 //2. view data via postman DELETE: localhost:4000/api/employees/:id
-router.delete("/:id", async function(req, res, next) {
+router.delete("/employees/:id", async function(req, res, next) {
   //let id = req.params.id
-  let { id } = req.params;
-  console.log(req.params);
+  const { id } = req.params;
+  // console.log(req.params.id);
   try {
     //delete students with specified id from database
-    await db(`DELETE FROM personal_info WHERE id = ${id};`); 
+    await db(`DELETE FROM personal_info WHERE employeeId = ${id};`); 
      //delete students with specified id from database
-     await db(`DELETE FROM work_info WHERE id = ${id};`); 
+     await db(`DELETE FROM work_info WHERE employeeId = ${id};`); 
     //get the updated list of students
-    let results = await db('SELECT p.employeeId, p.fullName, p.address, p.country, p.passport, p.emailAddress, p.birthDate, p.phoneNumber, p.maritalStatus, w.department, w.epfNumber, w.SOCSO, w.startDate FROM personal_info p JOIN work_info w ON p.employeeID = w.employeeId;"');
+    let results = await db('SELECT p.employeeId, p.fullName, p.address, p.country, p.passport, p.emailAddress, p.birthDate, p.phoneNumber, p.maritalStatus, w.department, w.epfNumber, w.SOCSO, w.startDate FROM personal_info p JOIN work_info w ON p.employeeID = w.employeeId;');
     res.send(results.data);
   } catch (err) {
-    res.status(500).send({ error: err.message });
+    res.status(500).send({ error: "Deletion failed"  });
   }
 });
-
-// // DELETE an employee
-// //1. write syntax for DELETE FROM personal via mySQL and copy paste here
-// //2. view data via postman DELETE: localhost:5000/api/students/:id
-// router.delete("/:id", async function(req, res, next) {
-//   //your code here
-//   //let id = req.params.id
-//   let { id } = req.params;
-//   //console.log(req.params);
-//   try {
-//     //delete students with specified id from database
-//     await db(`DELETE FROM work_info WHERE id = ${id};`); 
-//     //get the updated list of students
-//     let results = await db('SELECT * FROM work_info');
-//     res.send(results.data);
-//   } catch (err) {
-//     res.status(500).send({ error: err.message });
-//   }
-// });
-
-module.exports = router;
-
 
 module.exports = router;
